f65777776a3b7eb73c9495db925a6e396add01c0
sfefelov <95582976+sfefelov@users.noreply.github.com>
2022-03-21 01:36:10 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -37,11 +37,26 @@ systemctl daemon-reload
 systemctl start shadowsocks-libev
 systemctl enable shadowsocks-libev
 
+!!!apt install simple-obfs
+nano /etc/shadowsocks-libev/config.json
+#"plugin":"obfs-server",
+#"plugin_opts":"obfs=http;failover=204.79.197.200:80"
+or
+#"plugin":"obfs-local",
+#"plugin_opts":"obfs=tls;failover=204.79.197.200:443"
+systemctl restart shadowsocks-libev
+
 passwd kali
 systemctl restart sshd
 systemctl enable sshd
 
 apt install stunnel4
+It is just a matter of editing file /etc/ssl/openssl.cnf changing last line
+from:
+CipherString = DEFAULT@SECLEVEL=2
+to
+CipherString = DEFAULT@SECLEVEL=1
+
 mkdir /var/lib/stunnel4/certs
 mkdir /var/lib/stunnel4/crls
 touch /var/lib/stunnel4/stunnel.log
@@ -52,12 +67,13 @@ nano /etc/stunnel/stunnel.conf
 #accept = 8989
 #connect = mail.ru:443
 
-openssl req -nodes -new -days 365 -newkey rsa:1024 -x509 -keyout serverkey.pem -out servercert.pem
+openssl req -nodes -new -days 365 -newkey rsa:2048 -x509 -keyout serverkey.pem -out servercert.pem
 mv serverkey.pem /etc/stunnel/
 mv servercert.pem /etc/stunnel/
 systemctl start stunnel4
 systemctl enable stunnel4 
-
+cd /etc/stunnel
+openssl req -newkey rsa:2048 -nodes -x509 -days 3650 -subj "/CN=server" -out servcert.pem -keyout serverkey.pem
 
 
 
